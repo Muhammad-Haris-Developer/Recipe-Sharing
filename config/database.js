71570577999c15d31cbad57a5396ca36e6bc0901
Muhammad-Haris-Developer <muhammad.haris@allstartech.co>
2024-12-30 18:17:11 +0500

    update database

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // const MONGO_URL = "mongodb+srv://mh00167009:Dj0RpXeR3RsYwGhQ@cluster0.9z6mv.mongodb.net/";
-const MONGO_URL = "mongodb+srv://mh00167009:Dj0RpXeR3RsYwGhQ@cluster0.9z6mv.mongodb.net/?retryWrites=true&w=majority";
+const MONGO_URL = "mongodb+srv://mh00167009:Dj0RpXeR3RsYwGhQ@cluster0.9z6mv.mongodb.net/Recipe_Sharing?retryWrites=true&w=majority";
 
 // const mongoConnect = async (callback) => {
 //     try{
