bfeb363b98f861bc9d8d0045d6819e9477eb6c2f
Muhammad-Haris-Developer <muhammad.haris@allstartech.co>
2024-12-30 17:18:37 +0500

    Database error

--- config/database.js
+++ config/database.js
@@ -1,18 +1,34 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = "mongodb+srv://mh00167009:Dj0RpXeR3RsYwGhQ@cluster0.9z6mv.mongodb.net/";
+// const MONGO_URL = "mongodb+srv://mh00167009:Dj0RpXeR3RsYwGhQ@cluster0.9z6mv.mongodb.net/";
+const MONGO_URL = "mongodb+srv://mh00167009:Dj0RpXeR3RsYwGhQ@cluster0.9z6mv.mongodb.net/?retryWrites=true&w=majority";
 
+// const mongoConnect = async (callback) => {
+//     try{
+//         await mongoose.connect(MONGO_URL, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true
+//         });
+//         console.log('Connected to MongoDB with Mongoose');
+//         callback();
+//     }
+//     catch(err){
+//         console.log("MongoDB connection failded: ", err);
+//     }
+// }
 const mongoConnect = async (callback) => {
     try{
         await mongoose.connect(MONGO_URL, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000
         });
         console.log('Connected to MongoDB with Mongoose');
         callback();
     }
     catch(err){
-        console.log("MongoDB connection failded: ", err);
+        console.log("MongoDB connection failed: ", err);
     }
 }
 
