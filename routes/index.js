54594d351978b16b342a5320b50d2990d3b560bc
Muhammad-Haris-Developer <muhammad.haris@allstartech.co>
2024-12-30 18:51:44 +0500

    index 3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('./index.html', { root: 'views' });
+  res.status(200).json({
+    status: 200,
+    message: 'Server Started Successfully!'
+  })
 });
 
 module.exports = router;
